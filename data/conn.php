96f68e03c249fcf2b9b8d0db1d9a7eb15b472a24
Matt Desimini <mdesimini@yahoo.com>
2016-11-13 22:33:58 -0500

    Removed Old Admin/Login Section

--- data/conn.php
+++ data/conn.php
@@ -1,9 +1,13 @@
 <?php
 
-$db_host = " "; 
-$db_username = " ";
-$db_password = " ";
-$db_name = " ";
+
+
+
+$db_host = "localhost"; //can be "localhost" for local development
+$db_username = "cs691-dps";
+$db_password = "Xahcigh2up9j";
+$db_name = "f13-it691-s01";
+$db_name = "cs691-dps";
 
 $conn = new mysqli($db_host,$db_username,$db_password,$db_name) or die(mysqli_error());
 
